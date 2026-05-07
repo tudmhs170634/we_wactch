19e3b0ea970f2b7bc249ab2e3cef784f6cbce66e
trungnthe <144328061+trungnthe@users.noreply.github.com>
2026-05-08 01:16:34 +0700

    fix: resolve remaining merge conflict in room.module.ts

--- we-wactch-server/src/room/room.module.ts
+++ we-wactch-server/src/room/room.module.ts
@@ -1,23 +1,12 @@
 import { Module } from '@nestjs/common';
-<<<<<<< HEAD
-import { PrismaModule } from '../prisma/prisma.module';
 import { RoomController } from './room.controller';
 import { RoomService } from './room.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
   controllers: [RoomController],
   providers: [RoomService],
-=======
-import { RoomController } from './room.controller';
-import { RoomService } from './room.service';
-import { PrismaModule } from '../prisma/prisma.module';
-
-@Module({
-    imports: [PrismaModule],
-    controllers: [RoomController],
-    providers: [RoomService],
-    exports: [RoomService],
->>>>>>> origin/client-server
+  exports: [RoomService],
 })
 export class RoomModule {}
